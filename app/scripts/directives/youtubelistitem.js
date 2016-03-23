8a174f9d15818c933146aa2945794bba95fdd2b4
codingmaven <codingmaven@gmail.com>
2016-03-23 12:30:34 -0700

    codingstyle fix

--- app/scripts/directives/youtubelistitem.js
+++ app/scripts/directives/youtubelistitem.js
@@ -9,18 +9,18 @@
 angular.module('youtubeApp')
   .directive('youtubeListItem', function (youtubeService) {
     return {
-      templateUrl: '../../views/youtubelistitem.html',
+      templateUrl: 'views/youtubelistitem.html',
       restrict: 'E',
       scope: {
         data: '@',
         index: '@'
       },
-      link: function postLink(scope, element, attrs) {
+      link: function postLink(scope) {
         scope.videoData = JSON.parse(scope.data);
         scope.clicked = function(){
           youtubeService.selectedVideo = youtubeService.videoList[scope.index];
           console.log(scope.index);
-        }
+        };
         console.log(scope.data);
       }
     };
--- test/spec/directives/youtubelistitem.js
+++ test/spec/directives/youtubelistitem.js
@@ -5,8 +5,7 @@ describe('Directive: youtubeListItem', function () {
   // load the directive's module
   beforeEach(module('youtubeApp'));
 
-  var element,
-    scope;
+  var scope;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
